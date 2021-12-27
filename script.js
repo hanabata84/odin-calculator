bd535be720bf318849a5927cc0d5bf89f0c893d4
hanabata84 <dianneerika.busa@gmail.com>
2021-12-27 18:06:58 +0900

    calculator functions ver 1.0

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
+const para = document.querySelector('p');
+const btns = document.querySelectorAll('.btn');
+let val1 = '';
+let val2 = '';
+let op = '';
+
+btns.forEach(btn => btn.addEventListener('click', function(){
+    const dataButton = btn.dataset.btn;
+    if(dataButton === 'add' || dataButton === 'sub' || dataButton === 'mul' || dataButton === 'div'){
+        if(op !== ''){
+            para.textContent = operate(op,Number(val1),Number(val2));
+            val1 = para.textContent;
+            op = dataButton;
+            val2 = '';
+        }else {
+            op = dataButton;
+        }
+        
+    } else if(dataButton === 'equal'){
+        para.textContent = operate(op,Number(val1),Number(val2));
+    } else if(dataButton === 'clear'){
+        val1 = '';
+        val2 = '';
+        op = '';
+        para.textContent = '';
+    }
+    else{
+        if(op !== ''){
+            if(val2 !== ''){
+                para.textContent += btn.textContent;
+                val2 = para.textContent;
+            }else{
+                para.textContent = '';
+                para.textContent += btn.textContent;
+                val2 = para.textContent;
+            }
+            
+        } else {
+            para.textContent += btn.textContent;
+            val1 = para.textContent;
+        }        
+    }    
+}));
+
 function add(a, b){
     return a + b;
 }
@@ -17,11 +61,11 @@ function divide(a, b){
 function operate(operator, a, b){
     if(operator === 'add'){
         return add(a, b);
-    }else if(operator === 'subtract'){
+    }else if(operator === 'sub'){
         return subtract(a, b);
-    }else if(operator === 'multiply'){
+    }else if(operator === 'mul'){
         return multiply(a, b);
-    }else if(operator === 'divide'){
+    }else if(operator === 'div'){
         return divide(a, b);
     }
 }
